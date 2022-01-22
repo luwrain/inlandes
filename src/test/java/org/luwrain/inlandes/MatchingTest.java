105d2b63d29be8adf1558fdcf9985a1b134a59f1
Michael Pozhidaev <msp@luwrain.org>
2022-01-22 14:27:42 +0700

    Adding MatchingTest.nestedBlocksSingleWordWithRef()

--- src/test/java/org/luwrain/inlandes/MatchingTest.java
+++ src/test/java/org/luwrain/inlandes/MatchingTest.java
@@ -25,9 +25,8 @@ import static org.luwrain.inlandes.util.Token.*;
 
 public class MatchingTest extends Assert
 {
-        static private final org.luwrain.inlandes.util.Token[] TEXT = tokenize("Это был замечательный день весны, который создавал настроение и вдохновлял на прекрасное.");
+    static private final org.luwrain.inlandes.util.Token[] TEXT = tokenize("Это был замечательный день весны, который создавал настроение и вдохновлял на прекрасное.");
     private SyntaxParser parser = null;
-
 
     @Test public void blockSingleWordWithRef()
     {
@@ -43,9 +42,35 @@ public class MatchingTest extends Assert
 	assertEquals(1, res.length);
 	assertEquals(res[0].getRule(), rr[0]);
 	assertFalse(res[0].getRefBegin(1) == NO_REF);
-		assertFalse(res[0].getRefEnd(1) == NO_REF);
-		assertEquals("день", text(copyOfRange(TEXT, res[0].getRefBegin(1), res[0].getRefEnd(1))));
+	assertFalse(res[0].getRefEnd(1) == NO_REF);
+	assertTrue(res[0].getRefEnd(1) > res[0].getRefBegin(1));
+	assertEquals("день", text(copyOfRange(TEXT, res[0].getRefBegin(1), res[0].getRefEnd(1))));
     }
+
+    @Test public void nestedBlocksSingleWordWithRef()
+    {
+	final RuleStatement[] rr = parser.parse("RULE WHERE {{день}_1}_2");
+	assertNotNull(rr);
+	assertEquals(1, rr.length);
+	assertNotNull(rr[0]);
+	assertNotNull(rr[0].getWhere());
+	assertEquals(1, rr[0].getWhere().items.length);
+	final Matcher m = new Matcher(rr);
+	final Matching[] res = m.match(TEXT);
+	assertNotNull(res);
+	assertEquals(1, res.length);
+	assertEquals(res[0].getRule(), rr[0]);
+	for(int i = 1; i< 3;i++)
+	{
+	    assertFalse(res[0].getRefBegin(i) == NO_REF);
+	    assertFalse(res[0].getRefEnd(i) == NO_REF);
+	    assertTrue(res[0].getRefEnd(i) > res[0].getRefBegin(i));
+	    assertEquals("день", text(copyOfRange(TEXT, res[0].getRefBegin(i), res[0].getRefEnd(i))));
+	}
+
+		
+    }
+
 
     @Before public void createParser()
     {

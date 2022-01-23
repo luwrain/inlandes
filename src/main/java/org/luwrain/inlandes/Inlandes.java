0278c7bf79d61c92e7ed3aed36932ee005edcea8
Michael Pozhidaev <msp@luwrain.org>
2022-01-23 13:05:57 +0700

    Adding the cli package

---
+++ src/main/java/org/luwrain/inlandes/Inlandes.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2021-2022 Michael Pozhidaev <msp@luwrain.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.luwrain.inlandes;
+
+import java.util.*;
+import static java.util.Arrays.*;
+import java.io.*;
+
+public final class Inlandes
+{
+    private final Map<String, Set<String>> dicts = new HashMap<>();
+    private final List<RuleStatement> rules = new ArrayList<>();
+    private final SyntaxParser parser = new SyntaxParser();
+
+    public void loadStandardLibrary()
+    {
+	try {
+	    dicts.put("roman", new HashSet<>(asList(readTextResourceAsArray("roman.dict", "UTF-8"))));
+	}
+	catch(IOException e)
+	{
+	    throw new RuntimeException(e);
+	}
+    }
+	
+
+    public void loadFile(String fileName, String charset) throws IOException
+    {
+	rules.addAll(Arrays.asList(parser.parse(readTextFile(fileName, charset))));
+    }
+
+    public void loadFile(String fileName) throws IOException
+    {
+	loadFile(fileName, "UTF-8");
+    }
+
+    private String readTextFile(String fileName, String charset) throws IOException
+    {
+	try (final BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), charset))){
+	    String line = r.readLine();
+	    final StringBuilder b = new StringBuilder();
+	    while (line != null)
+	    {
+		if (!line.trim().isEmpty() && line.trim().charAt(0) != '#')
+		b.append(line);
+		b.append(System.lineSeparator());
+		line = r.readLine();
+	    }
+	    return new String(b);
+	}
+    }
+
+        private String[] readTextFileAsArray(String fileName, String charset) throws IOException
+    {
+	try (final BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), charset))){
+	    String line = r.readLine();
+	    final List<String> res = new ArrayList<>();
+	    while (line != null)
+	    {
+		if (!line.trim().isEmpty() && line.trim().charAt(0) != '#')
+		    res.add(line);
+		line = r.readLine();
+	    }
+	    return res.toArray(new String[res.size()]);
+	}
+    }
+
+            private String[] readTextResourceAsArray(String resName, String charset) throws IOException
+    {
+	try (final BufferedReader r = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(resName), charset))){
+	    String line = r.readLine();
+	    final List<String> res = new ArrayList<>();
+	    while (line != null)
+	    {
+		if (!line.trim().isEmpty() && line.trim().charAt(0) != '#')
+		    res.add(line);
+		line = r.readLine();
+	    }
+	    return res.toArray(new String[res.size()]);
+	}
+    }
+
+    
+
+
+    
+}
+    
